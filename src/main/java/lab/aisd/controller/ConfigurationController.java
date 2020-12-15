98ee2965f56872b18321dace9ae60d6dc531c46e
Adrian <a.milewski@onet.eu>
2020-12-15 10:51:20 +0100

    Added settings & configuration

---
+++ src/main/java/lab/aisd/controller/ConfigurationController.java
@@ -0,0 +1,22 @@
+package lab.aisd.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleGroup;
+
+public class ConfigurationController {
+
+    @FXML
+    private RadioButton animationRb;
+
+    @FXML
+    private ToggleGroup displayOpt;
+
+    @FXML
+    private RadioButton logsRb;
+
+    @FXML
+    private Slider speedSlider;
+
+}
--- src/main/java/lab/aisd/controller/MapController.java
+++ src/main/java/lab/aisd/controller/MapController.java
@@ -6,6 +6,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.stage.FileChooser;
@@ -21,9 +22,6 @@ import java.util.ResourceBundle;
 public class MapController implements Initializable {
 
     private MusicPlayer musicPlayer = MusicPlayer.getInstance();
-
-    @FXML
-    private Slider volumeSlider;
 
     @FXML
     private MenuItem newMenuIt;
@@ -42,6 +40,15 @@ public class MapController implements Initializable {
 
     @FXML
     private MenuItem closeMenuIt;
+
+    @FXML
+    private Button startBt;
+
+    @FXML
+    private Button configBt;
+
+    @FXML
+    private Button settingsBt;
 
     @FXML
     void delete(ActionEvent event) {
@@ -75,20 +82,23 @@ public class MapController implements Initializable {
         Platform.exit();
     }
 
+    @FXML
+    void openConfig(ActionEvent event) {
+        StageManager.getInstance().openNewScene(FxmlView.CONFIG);
+    }
+
+    @FXML
+    void openSettings(ActionEvent event) {
+        StageManager.getInstance().openNewScene(FxmlView.SETTINGS);
+    }
+
+    @FXML
+    void startCalc(ActionEvent event) {
+        // Main calculations of the program (Business logic)
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         StageManager.getInstance().setResizable(true);
-        initSlider();
-    }
-
-    private void initSlider() {
-        volumeSlider.setValue(musicPlayer.getVolume());
-
-        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                musicPlayer.setVolume(newValue.doubleValue());
-            }
-        });
     }
 }
