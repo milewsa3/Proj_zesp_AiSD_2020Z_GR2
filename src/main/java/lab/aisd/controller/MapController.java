4b0053dd1000dd8ff6a4bcd30d89e18b11235f1b
Adrian <a.milewski@onet.eu>
2020-12-17 09:29:36 +0100

    Alert popups update

--- src/main/java/lab/aisd/controller/MapController.java
+++ src/main/java/lab/aisd/controller/MapController.java
@@ -1,27 +1,31 @@
 package lab.aisd.controller;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
-import lab.aisd.Main;
+import lab.aisd.model.Patient;
 import lab.aisd.util.FxmlView;
 import lab.aisd.util.MusicPlayer;
 import lab.aisd.util.StageManager;
+import lab.aisd.util.input.InputData;
+import lab.aisd.util.input.InputFileReader;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MapController implements Initializable {
 
     private MusicPlayer musicPlayer = MusicPlayer.getInstance();
+    private InputFileReader fileReader = new InputFileReader();
+    private InputData mapData;
+    private List<Patient> patientsData;
 
     @FXML
     private MenuItem newMenuIt;
@@ -51,6 +55,9 @@ public class MapController implements Initializable {
     private Button settingsBt;
 
     @FXML
+    private AnchorPane mainArea;
+
+    @FXML
     void delete(ActionEvent event) {
 
     }
@@ -59,12 +66,59 @@ public class MapController implements Initializable {
     void loadMap(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         File selectedFile = fileChooser.showOpenDialog(StageManager.getInstance().getStage());
+        if (selectedFile == null)
+            return;
+
+        try {
+            mapData = fileReader.readMainFile(selectedFile.getPath());
+            generateMap();
+            showDataLoadedSuccessfullyAlert();
+
+        } catch (Exception e) {
+            StageManager.getInstance().showAlertScene(
+                    Alert.AlertType.ERROR,
+                    "Error",
+                    "Reading map file error",
+                    e.getMessage());
+        }
+    }
+
+    private void generateMap() {
+        System.out.println(mainArea.getWidth());
     }
 
     @FXML
     void loadPatients(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         File selectedFile = fileChooser.showOpenDialog(StageManager.getInstance().getStage());
+        if (selectedFile == null)
+            return;
+
+        try {
+            patientsData = fileReader.readPatientsFile(selectedFile.getPath());
+            generatePatientsOnMap();
+            showDataLoadedSuccessfullyAlert();
+
+        } catch (Exception e) {
+            StageManager.getInstance().showAlertScene(
+                    Alert.AlertType.ERROR,
+                    "Error",
+                    "Reading patients file error",
+                    e.getMessage());
+        }
+    }
+
+    private void showDataLoadedSuccessfullyAlert() {
+        StageManager.getInstance().showAlertScene(
+                Alert.AlertType.INFORMATION,
+                "Information",
+                "Data loaded successfully",
+                "Great. Your data is valid and it was loaded properly."
+        );
+    }
+
+    private void generatePatientsOnMap() {
+
     }
 
     @FXML
@@ -95,10 +149,45 @@ public class MapController implements Initializable {
     @FXML
     void startCalc(ActionEvent event) {
         // Main calculations of the program (Business logic)
+        if (!userWantsToStartCalc() || !isDataValid())
+            return;
+
+        System.out.println("Calc started");
+    }
+
+    private boolean userWantsToStartCalc() {
+        Optional<ButtonType> result = StageManager.getInstance().showAlertScene(
+                Alert.AlertType.CONFIRMATION,
+                "Confirmation Dialog",
+                "Confirmation",
+                "Are you sure you want to start calculations?");
+
+        return result.get() == ButtonType.OK;
+    }
+
+    private boolean isDataValid() {
+        if (mapData != null && patientsData != null) {
+            return true;
+        }
+        else {
+            StageManager.getInstance().showAlertScene(
+                    Alert.AlertType.ERROR,
+                    "Error",
+                    "Data issue",
+                    "Data is not loaded"
+            );
+
+            return false;
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         StageManager.getInstance().setResizable(true);
+        initAddingPatientsOnDoubleClick();
+    }
+
+    private void initAddingPatientsOnDoubleClick() {
+
     }
 }
